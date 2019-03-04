53d64d9cb350ac1868ab4bb0b92fb51a7de30579
viktor <ovramenko.victor@gmail.com>
2019-03-04 21:50:16 +0200

    second commit about  PageObject2

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,13 +3,26 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import java.awt.*;
+
+import static org.openqa.selenium.By.xpath;
 
 public class LoginPage extends ParentPage{
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
+
+    @FindBy(name = "email")
+    private WebElement inputEmail;
+
+    @FindBy (name = "password")
+    private WebElement inputPass;
+
+    @FindBy (xpath = (".//button[@type = 'submit']"))
+    private WebElement button;
 
     public void openPage() {
         try {
@@ -22,32 +35,36 @@ public class LoginPage extends ParentPage{
     }
 
     public void enterTextInToInputLogin(String login) {
-        try{
-            webDriver.findElement(By.name("email")).clear();
-            webDriver.findElement(By.name("email")).sendKeys(login);
-            System.out.println(login + "was input");
-        }catch (Exception e){
-            System.out.println("Can not work element" + e);
-            Assert.fail("Can not work element" + e);
-        }
-    } public void enterTextInToInputPass(String pass) {
-        try{
-            webDriver.findElement(By.name("password")).clear();
-            webDriver.findElement(By.name("password")).sendKeys(pass);
-            System.out.println(pass + "was input");
-        }catch (Exception e){
-            System.out.println("Can not work element" + e);
-            Assert.fail("Can not work element" + e);
-        }
+//        try{
+//            inputEmail.clear();
+//            inputEmail.sendKeys(login);
+//            System.out.println(login + "was input");
+//        }catch (Exception e){
+//            System.out.println("Can not work element" + e);
+//            Assert.fail("Can not work element" + e);
+//        }
+        actionsWithOurElements.enterTextInToElement(inputEmail, login);
+    }
+    public void enterTextInToInputPass(String pass) {
+//        try{
+//            inputPass.clear();
+//            inputPass.sendKeys(pass);
+//            System.out.println(pass + "was input");
+//        }catch (Exception e){
+//            System.out.println("Can not work element" + e);
+//            Assert.fail("Can not work element" + e);
+//        }
+        actionsWithOurElements.enterTextInToElement(inputPass, pass);
     }
     public void clickOnButtonSubmit(){
-        try{
-            webDriver.findElement(By.xpath(".//button[@type = 'submit']")).click();
-            System.out.println("Button Submit was clicked");
-
-        }catch (Exception e){
-            System.out.println("Can not work element" + e);
-            Assert.fail("Can not work element" + e);
-        }
+//        try{
+//            button.click();
+//            System.out.println("Button Submit was clicked");
+//
+//        }catch (Exception e){
+//            System.out.println("Can not work element" + e);
+//            Assert.fail("Can not work element" + e);
+//        }
+        actionsWithOurElements.clickONElement(button);
     }
 }
